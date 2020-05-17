e58dcf85fc98903e7cc7d393eb48348fdc73d5cf
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-05-17 12:24:11 +0200

    add demo bin

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -1,10 +1,10 @@
 #include <pangolin/game-object.hpp>
 #include <pangolin/text-renderer.hpp>
-#include <pangolin/resource-manager.hpp>
+/* #include <pangolin/resource-manager.hpp> */
 
 int main() {
   pgl::GameObject go;
-  pgl::loader::Shader shader
-    = pgl::loader::ResourceManager::load_shader("", "", "", "");
+  /* pgl::loader::Shader shader */
+  /*   = pgl::loader::ResourceManager::load_shader("", "", "", ""); */
   return 0;
 }
