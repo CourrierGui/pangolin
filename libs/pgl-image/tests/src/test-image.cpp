246c01cbcf0622bd40bfdf38b0eb205d5bf96927
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-05-17 12:46:34 +0200

    gtest integration

--- libs/pgl-image/tests/src/test-image.cpp
+++ libs/pgl-image/tests/src/test-image.cpp
@@ -1,6 +1,12 @@
 #include <pgl-image/image-loader.hpp>
+#include <gtest/gtest.h>
 
-int main() {
+TEST(PglImageTest, LoadPNG) {
   pgl::loader::load_png("../tests/resources/awesomeface.png");
   pgl::loader::load_png("../tests/resources/paddle.png");
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
