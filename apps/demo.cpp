b9b1d1aeef33a622f9afde87a66a526e1beb3d87
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-05-27 17:09:46 +0200

    implementation of a simple demo window

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -1,10 +1,13 @@
-#include <pangolin/game-object.hpp>
-#include <pangolin/text-renderer.hpp>
-/* #include <pangolin/resource-manager.hpp> */
+#include <pgl-gui/glfw-window.hpp>
 
 int main() {
-  pgl::GameObject go;
-  /* pgl::loader::Shader shader */
-  /*   = pgl::loader::ResourceManager::load_shader("", "", "", ""); */
+	pgl::gui::GLFWWindow& window = pgl::gui::GLFWWindow::make_window(600, 400, "Pangolin demo");
+
+	while (window.is_open()) {
+		window.poll_events();
+		window.clear();
+
+		window.swap_buffer();
+	}
   return 0;
 }
