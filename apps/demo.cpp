8c31ed4dc4aeb1073e65cd8ec8c664dba571a8a5
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-04-10 21:06:37 +0200

    demo: use float2

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -1,6 +1,7 @@
 #include <pgl-gui/glfw-window.hpp>
 #include <pgl-core/resource-manager.hpp>
 #include <pgl-core/text-renderer.hpp>
+#include <pgl-math/vector.hpp>
 
 #include <array>
 #include <iostream>
@@ -40,14 +41,12 @@ int main() {
 
   pgl::gui::TextRenderer renderer{
     width, height,
-      pgl::ResourceManager::get_shader("text")};
+    pgl::ResourceManager::get_shader("text")};
   renderer.load("../resources/fonts/ocraext.TTF", 14);
 
   std::string text = "Hello World!";
-  float x = width/2.0f;
-  float y = height/2.0f;
-  float xpos = x;
-  float ypos = y;
+  pgl::float2 pos{width/2.0f, height/2.0f};
+  pgl::float2 cursorPos = pos;
 
   window.set_key_cb(
     [&text](pgl::gui::GLFWWindow& window, int key, int scancode, int action, int mods) {
@@ -55,30 +54,23 @@ int main() {
     });
 
   window.set_mouse_button_cb(
-    [&x, &y, &xpos, &ypos](pgl::gui::GLFWWindow& window, int button, int action, int mode)
+    [&pos, &cursorPos](pgl::gui::GLFWWindow&, int button, int action, int)
     -> void
     {
       if (action != GLFW_PRESS) return;
 
       if (button == GLFW_MOUSE_BUTTON_LEFT) {
-        x = xpos;
-        y = ypos;
+        pos = cursorPos;
       }
     });
 
-  window.set_cursor_cb(
-    [&xpos, &ypos](pgl::gui::GLFWWindow&, float x, float y)
-    -> void
-    {
-      xpos = x;
-      ypos = y;
-    });
+  window.set_cursor_cb([&cursorPos](pgl::gui::GLFWWindow&, float x, float y) -> void { cursorPos = {x, y}; });
 
   while (window.is_open()) {
     window.poll_events();
     window.clear(1.0f, 1.0f, 1.0f, 1.0f);
 
-    renderer.render_text(text, x, y, 1.0f, pgl::float3{0.0f, 0.0f, 0.0f});
+    renderer.render_text(text, pos.x, pos.y, 1.0f, pgl::float3{0.0f, 0.0f, 0.0f});
 
     window.swap_buffer();
   }
