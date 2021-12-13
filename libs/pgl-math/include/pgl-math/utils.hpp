787445152bf6ae95a10f7411d000b87c7ef051e6
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-12-13 22:49:22 +0100

    pgl-math: uncomment concept requirements and fix style issues

--- libs/pgl-math/include/pgl-math/utils.hpp
+++ libs/pgl-math/include/pgl-math/utils.hpp
@@ -7,39 +7,41 @@
 
 namespace pgl {
 
-  using length = std::uint32_t;
+    using length = std::uint32_t;
 
-  template<typename T>
-    concept integral = std::is_arithmetic_v<T>;
+    template<typename T>
+        concept integral = std::is_arithmetic_v<T>;
 
-  template<typename T>
-    concept number = std::is_arithmetic_v<T> && !std::is_same_v<T,bool>;
+    template<typename T>
+        concept number = std::is_arithmetic_v<T> && !std::is_same_v<T,bool>;
 
-  template<typename T>
-    concept container =
-    requires(T a) {
-      /* typename T::value_type; */
-      /* typename T::size_type; */
-      /* typename T::iterator; */
-      /* typename T::const_iterator; */
-      {a.size()  };
-      {a.begin() };
-      {a.end()   };
-      /* {a.cbegin()}; */
-      /* {a.cend()  }; */
-    };
+    template<typename T>
+        concept container =
+        requires(T a) {
+            typename T::value_type;
+            typename T::size_type;
+            typename T::iterator;
+            typename T::const_iterator;
+            {a.size()  };
+            {a.begin() };
+            {a.end()   };
+            {a.cbegin()};
+            {a.cend()  };
+        };
 
-  constexpr float  pi  {std::numbers::pi_v<float>};
-  constexpr double dpi {std::numbers::pi_v<double>};
+    constexpr float pi{std::numbers::pi_v<float>};
+    constexpr double dpi{std::numbers::pi_v<double>};
 
-  template<std::floating_point type>
-    type radians(type degrees) {
-      return std::numbers::pi_v<type>*degrees/180;
-    }
+    template<std::floating_point type>
+        type radians(type degrees)
+        {
+            return std::numbers::pi_v<type> * degrees / 180;
+        }
 
-  template<std::floating_point type>
-    type degrees(type radians) {
-      return 180*radians/std::numbers::pi_v<type>;
-    }
+    template<std::floating_point type>
+        type degrees(type radians)
+        {
+            return 180 * radians / std::numbers::pi_v<type>;
+        }
 
 } /* end of namespace pgl */
